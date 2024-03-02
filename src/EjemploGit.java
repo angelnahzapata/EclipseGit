f6d1dbe4c0ebd5ac6356fc91967fff2a7fd861ec
angel <nah834086@gmail.com>
2024-03-01 21:57:14 -0600

    Puse la edad

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,11 +4,15 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Hola a todos");
+		System.out.print("Hola a todos ");
 		
 		String nombre = "Angel";
 		
 		System.out.println(nombre);
+		
+		String edad = "21";
+		
+		System.out.println(edad);
 
 	}
 
